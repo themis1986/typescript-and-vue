6ad697cd0d7164e72b1a0d238ee9553582e79b0d
thiakovidis <thiakovidis@agroapps.gr>
2022-12-10 22:06:29 +0200

    Migrate Dish Card to script setup

--- app/src/components/DishCard.vue
+++ app/src/components/DishCard.vue
@@ -1,35 +1,33 @@
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
 import type { PropType } from 'vue'
 import type { Dish } from '@/types'
+import { computed } from 'vue'
 
-export default {
-  props: {
-    dish: {
-      type: Object as PropType<Dish>,
-      required: true,
-    },
-  },
-  emits: ['delete-dish'],
-  computed: {
-    statusColor() {
-      switch (this.dish.status) {
-        case 'Want to Try':
-          return 'is-warning'
-        case 'Recommended':
-          return 'is-success'
-        case 'Do Not Recommend':
-          return 'is-danger'
-        default:
-          return ''
-      }
-    },
-  },
-  methods: {
-    deleteDish() {
-      this.$emit('delete-dish', this.dish)
-    },
-  },
+type PropTypes = {
+  dish: Dish
+}
+
+const props = defineProps<PropTypes>()
+
+const emit = defineEmits<{
+  (e: 'delete-dish', value: Dish): void
+}>()
+
+const statusColor = computed(() => {
+  switch (props.dish.status) {
+    case 'Want to Try':
+      return 'is-warning'
+    case 'Recommended':
+      return 'is-success'
+    case 'Do Not Recommend':
+      return 'is-danger'
+    default:
+      return ''
+  }
+})
+
+const deleteDish = () => {
+  emit('delete-dish', props.dish)
 }
 </script>
 
